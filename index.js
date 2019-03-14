b98750cbb7ad8fb957f9ceaed3c787ba1104bcf2
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-03-14 08:33:45 -0500

    Añadiendo proyecto back-end en NodeJS-Express

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var config = require('./config')
+const mongoose = require('mongoose')
+const app = require("./app")
+
+mongoose.connect(config.db,{ useCreateIndex: true,
+                             useNewUrlParser: true } , (err, res) => {
+    if (err) {
+    return console.log(`Error al conectar a la base de datos: ${err}`)
+  }
+  console.log('Conexión a la base de datos establecida...')
+    app.listen(config.port, function () {
+        console.log(`App web corriendo en ${config.port}`)
+    })
+});
