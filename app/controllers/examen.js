3188f91dbfe61d530f861c3afc7fa8aaab2c7b9f
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-04-03 17:50:03 -0500

    Añadiendo pre-started test view v1

---
+++ app/controllers/examen.js
@@ -0,0 +1,15 @@
+const Student = require('../models/student');
+const bcrypt = require('bcrypt');
+const service = require("../services")
+const fs = require('fs');
+const {check, validationResult} = require('express-validator/check');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+function loadPreStarted(req, res){
+    res.render('../views/test-prestarted/test-prestarted.ejs');
+}
+
+module.exports = {
+    loadPreStarted
+};
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -10,22 +10,20 @@ const mongoose = require('mongoose');
 
  //to encrypt
 const bcrypt = require('bcrypt');
-const userCtlr = require('../controllers/user');
+const adminCtlr = require('../controllers/admin');
 const studentCtrlr = require('./../controllers/student');
-const loginCtlr = require('../controllers/login')
+const loginCtlr = require('../controllers/login');
+const testCtlr = require('../controllers/examen');
 
  // It will contain all the end points
 const router = express.Router();
 
 const {check, validationResult} = require('express-validator/check');
 
-//router.get('/test/pre-started', userCtlr.loadTest);
-
-//router.get('/login/candidate', userCtlr.loadLoginCandidate);
-
 router.get('/signin/candidate', studentCtrlr.loadLoginCandidate);
-router.get('/signin/admin', userCtlr.loadLoginAdmin);
+router.get('/signin/admin', adminCtlr.loadLoginAdmin);
 router.get('/signin', loginCtlr.loadLogin);
+
 router.post('/signin/candidate', [
     check('doctype').isNumeric().isIn([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]), //validate,
     check('docnumber').isNumeric().isLength({min: 5})
@@ -42,6 +40,7 @@ router.post('/register/candidate', [
     check('phonenumber').isMobilePhone().isLength({max: 12}),
     check('email').isEmail().isLength({min: 7})
 ], studentCtrlr.register);
-router.get('/candidate/profile', studentCtrlr.userProfile);
-//router.get('/student/logout', studentCtrlr.logout);
+router.get('/candidate/profile', studentCtrlr.userProfile); // Carga el perfil del aspirante
+router.get('/candidate/test/pre-started', testCtlr.loadPreStarted); // Carga las instrucciones del examen
+//router.get('/student/logout', studentCtrlr.logout); // Cierra sesión del estudiante
 module.exports = router;
