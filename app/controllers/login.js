b06a4ff031ccfa4901213f1e6fcbddb3c2c181ee
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-04-03 15:43:17 -0500

    Pantalla inicial

---
+++ app/controllers/login.js
@@ -0,0 +1,15 @@
+const Student = require('../models/student');
+const bcrypt = require('bcrypt');
+const service = require("../services")
+const fs = require('fs');
+const {check, validationResult} = require('express-validator/check');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+function loadLogin(req, res){
+    res.render('../views/login/login.ejs');
+}
+
+module.exports = {
+    loadLogin
+};
