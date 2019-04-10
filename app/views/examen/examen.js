33b439bc249b1e91baca92d31482885b4450630c
Juan Pablo González <jugonz31@eafit.edu.co>
2019-04-10 11:05:17 -0500

    Retoques en el despliegue del examen

--- app/views/examen/examen.js
+++ app/views/examen/examen.js
@@ -7,9 +7,15 @@ req.onreadystatechange = function () {
     if (req.readyState == 4 && req.status == 200) {
         var texto = req.response;
         var title = texto.question.title;
-        console.log("title: ", title);
         document.getElementById("qidc").innerHTML = title;
         var opcionA = texto.question.responses[0];
+        opcionA = opcionA.substring(3,opcionA.lenght);
         document.getElementById("opcionA").innerHTML = opcionA;
+        var opcionB = texto.question.responses[1];
+        opcionB = opcionB.substring(3,opcionB.lenght);
+        document.getElementById("opcionB").innerHTML = opcionB;
+        var opcionC = texto.question.responses[2];
+        opcionC = opcionC.substring(3,opcionC.lenght);
+        document.getElementById("opcionC").innerHTML = opcionC;
     }
 }
