0591766de5e9ba6752d6fb749925e5d12dbcdbaf
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-04-14 13:56:23 -0500

    Añadiendo examen-results view

--- app/views/examen-results/examen-results.js
+++ app/views/examen-results/examen-results.js
@@ -7,11 +7,14 @@ req.onreadystatechange = function () {
     if (req.readyState == 4 && req.status == 200) {
         var nota = req.response;
         var basico = ((nota.part1 * 10) / 2).toFixed(1);
-        document.getElementById("part1").innerHTML = basico;
+        var aciertosBasico = (nota.part1 * 100).toFixed(2);
+        document.getElementById("part1").innerHTML = aciertosBasico;
         var intermedio = ((nota.part2 * 10) / 2).toFixed(1);
-        document.getElementById("part2").innerHTML = intermedio;
+        var aciertosIntermedio = (nota.part2 * 100).toFixed(2);
+        document.getElementById("part2").innerHTML = aciertosIntermedio;
         var avanzado = ((nota.part3 * 10) / 2).toFixed(1);
-        document.getElementById("part3").innerHTML = avanzado;
+        var aciertosAvanzado = (nota.part3 * 100).toFixed(2);
+        document.getElementById("part3").innerHTML = aciertosAvanzado;
         var notaFinal = (((parseFloat(basico)) + (parseFloat(intermedio))+ (parseFloat(avanzado))) / 3).toFixed(1);
         document.getElementById("final_result").innerHTML = notaFinal;
     }
