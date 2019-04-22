24133e46fcce628a6bcc3b5a94f87dfda52a2d07
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-04-21 22:24:10 -0500

    Actualización de datos del aspirante funcionando

--- app/views/candidate-update/candidate-update.js
+++ app/views/candidate-update/candidate-update.js
@@ -1,24 +1,64 @@
-var token = localStorage.getItem("mikey");
-console.log("token: ", token);
+var http = new XMLHttpRequest();
 var doc_number = localStorage.getItem("docnumber");
-console.log("docnumber", doc_number);
-var settings = {
-    "url": "/api/candidate/list",
-    "method": "GET",
-    "headers": {
-        "content-type": "application/json",
-        "authorization": token,
-    },
-    "data": JSON.stringify({
-        "docnumber": doc_number
-    })
-}
-$.ajax(settings).done(function (response) {
-    if (response.info_candidate.length != 0) {
-        console.log(response.info_candidate);
-    } else {
-        console.log("No tienes información almacenada");
+var params = 'docnumber='+doc_number;
+http.responseType = 'json';
+http.open('GET', '/api/candidate/list'+'?'+ params, true);
+http.setRequestHeader("Content-type", "application/json");
+http.send(null);
+http.onreadystatechange = function () {
+    if (http.readyState == 4 && http.status == 200) {
+        var texto = http.response.info_candidate;
+        var id = texto._id;
+        localStorage.setItem("_id", id);
+        var nombres = texto.firstname;
+        document.getElementById("firstName").value = nombres;
+        var apellidos = texto.lastname;
+        document.getElementById("lastName").value = apellidos;
+        var fecha_nacimiento = texto.birthdate;
+        document.getElementById("birthdate").value = fecha_nacimiento;
+        var direccion = texto.address;
+        document.getElementById("address").value = direccion;
+        var telefono = texto.phonenumber;
+        document.getElementById("telefono").value = telefono;
+        var celular = texto.mobilephonenumber;
+        document.getElementById("celular").value = celular;
+        var correo = texto.email;
+        document.getElementById("correo").value = correo;
+        var ciudad = texto.currentcity;
+        document.getElementById("city").value = ciudad;
+        var genero = texto.genre;
+        document.getElementById("genero").value = genero;
     }
-}).fail(function (err) {
-    console.log(err);
-});
+}
+
+let update = function () {
+    var http = new XMLHttpRequest();
+    var id = localStorage.getItem('_id');
+    var params = 'idCandidate='+id;
+    http.responseType = 'json';
+    http.open("PUT", "/api/candidate/update"+'?'+params, true); 
+    http.setRequestHeader("Content-type", "application/json");
+    http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+            localStorage.setItem("mikey", http.response.token);
+            window.location.replace('/candidate/test/pre_started');
+        }
+    }
+    var isChecked = document.getElementById("same-address").checked;
+    if(isChecked){
+    http.send(JSON.stringify({ doctype: localStorage.getItem('doctype'), 
+                                docnumber: localStorage.getItem('docnumber'),
+                                firstname: document.getElementById("firstName").value,
+                                lastname: document.getElementById("lastName").value,
+                                genre: document.getElementById("genero").value,
+                                birthdate: document.getElementById("birthdate").value,
+                                currentcity: document.getElementById("city").value,
+                                address: document.getElementById("address").value,
+                                phonenumber: document.getElementById("telefono").value,
+                                mobilephonenumber: document.getElementById("celular").value,
+                                email: document.getElementById("correo").value
+    }));
+    }else{
+        $('#failed').show();
+    }
+}
