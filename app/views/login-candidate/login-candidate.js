83fc14c7b398eee2e88c99facd1b383fbec19330
Juan Pablo González <jugonz31@eafit.edu.co>
2019-05-02 16:39:38 -0500

    Login sólo si esta activo el examen.

--- app/views/login-candidate/login-candidate.js
+++ app/views/login-candidate/login-candidate.js
@@ -1,19 +1,47 @@
 let login = function () {
     var http = new XMLHttpRequest();
     http.responseType = 'json';
-    http.open("POST", "/api/signin/candidate", true); 
+    http.open("POST", "/api/signin/candidate", true);
     http.setRequestHeader("Content-type", "application/json");
     http.onreadystatechange = function () {
         if (http.readyState == 4 && http.status == 200) {
             localStorage.setItem("docnumber", document.getElementById("docnumber").value);
             localStorage.setItem("doctype", document.getElementById("doctype").value);
             localStorage.setItem("mikey", http.response.token);
-            window.location.replace('/candidate/profile');
+            console.log("localstorage melos")
+            check();
         }
     }
-    http.send(JSON.stringify({ doctype: document.getElementById("doctype").value, 
-                        docnumber: document.getElementById("docnumber").value }));
+    http.send(JSON.stringify({
+        doctype: document.getElementById("doctype").value,
+        docnumber: document.getElementById("docnumber").value
+    }));
 }
+
+let check = function () {
+    setTimeout(function () {
+        var http2 = new XMLHttpRequest();
+        var doc_number = localStorage.getItem("docnumber");
+        var params = 'docnumber=' + doc_number;
+        http2.responseType = 'json';
+        http2.open('GET', '/api/candidate/list' + '?' + params, true);
+        http2.setRequestHeader("Content-type", "application/json");
+        http2.send(null);
+        http2.onreadystatechange = function () {
+            if (http2.readyState == 4 && http2.status == 200) {
+                var texto = http2.response.info_candidate;
+                console.log()
+                var examen_activo = texto.examen_activo;
+                if (!examen_activo) {
+                    window.location.replace('/candidate/test/error');
+                } else {
+                    window.location.replace('/candidate/profile');
+                }
+            }
+        }
+    }, 1000)
+}
+
 
 $(document).ready(function () {
     $('#failed').hide();
@@ -23,6 +51,6 @@ $(document).ready(function () {
 
         if (doctype == '' || docnumber == '') {
             $('#failed').show();
-        } 
+        }
     });
 });
