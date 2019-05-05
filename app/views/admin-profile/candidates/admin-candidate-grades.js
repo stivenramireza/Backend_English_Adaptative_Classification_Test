a0e7082f69b9153994479b8cbd8a12495aacf1e3
Juan Pablo González <jugonz31@eafit.edu.co>
2019-05-05 11:59:35 -0500

    Update admin-candidate-grades.js

--- app/views/admin-profile/candidates/admin-candidate-grades.js
+++ app/views/admin-profile/candidates/admin-candidate-grades.js
@@ -11,10 +11,10 @@ let getGrades = function () {
         if (req.readyState == 4 && req.status == 200) {
             var textoId = req.response.info_examen;
             document.getElementById('fec').innerHTML = new Date(textoId.fecha);
-            document.getElementById('pa1').innerHTML = textoId.part1;
-            document.getElementById('pa2').innerHTML = textoId.part2;
-            document.getElementById('pa3').innerHTML = textoId.part3;
-            document.getElementById('tot').innerHTML = textoId.grade;
+            document.getElementById('pa1').innerHTML = String(textoId.part1).substr(0,3);
+            document.getElementById('pa2').innerHTML = String(textoId.part2).substr(0,3);
+            document.getElementById('pa3').innerHTML = String(textoId.part3).substr(0,3);
+            document.getElementById('tot').innerHTML = String(textoId.grade).substr(0,3);
             document.getElementById('niv').innerHTML = textoId.classified_level;
             var array_respuestas = textoId.responses;
             var array_partes = textoId.parts;
@@ -22,7 +22,7 @@ let getGrades = function () {
             var b = new Date(textoId.hora_fin);
             var c = ((a - b) / 1000);
             console.log(c);
-            
+
             var http = new XMLHttpRequest();
             var params = 'docnumber=' + doc_number;
             http.responseType = 'json';
