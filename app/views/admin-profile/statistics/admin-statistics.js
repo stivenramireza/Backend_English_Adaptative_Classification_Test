b8c149e88bf8438a8247b32c7cc318b3be370aa8
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-05-14 11:35:15 -0500

    Añadiendo gráficas

--- app/views/admin-profile/statistics/admin-statistics.js
+++ app/views/admin-profile/statistics/admin-statistics.js
@@ -24,3 +24,96 @@ let queryStatistics = function () {
         }
     }
 }
+
+let getGraph = function () {
+    y.style.display = "block";
+    var tipo_grafica = document.getElementById("tipo_grafica").value;
+    document.getElementById("texto_span").innerHTML = "Porcentaje de aciertos";
+    if (tipo_grafica == '0') {
+        y.style.display = "none";
+    } else if (tipo_grafica == '1') {
+        x.style.display = "block";
+        Highcharts.chart('container', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: ''
+            },
+            xAxis: {
+                type: 'category',
+                labels: {
+                    rotation: -45,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Niveles de Clasificación'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: 'Porcentaje: <b>{point.y:.0f}%</b>'
+            },
+            series: [{
+                name: 'Porcentaje',
+                data: [
+                    ['Parte 1', 5],
+                    ['Parte 2', 6],
+                    ['Parte 3', 7],
+                ],
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    format: '{point.y:.0f}', // one decimal
+                    y: 17, // 10 pixels down from the top
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            }]
+        });
+    } else if (tipo_grafica == '2') {
+        x.style.display = "block";
+        document.getElementById("texto_span").innerHTML = "Porcentaje de aciertos";
+        Highcharts.chart('container', {
+            chart: {
+                type: 'pie',
+                options3d: {
+                    enabled: true,
+                    alpha: 45
+                }
+            },
+            title: {
+                text: ''
+            },
+            plotOptions: {
+                pie: {
+                    innerSize: 100,
+                    depth: 45
+                }
+            },
+            tooltip: {
+                pointFormat: 'Porcentaje: <b>{point.y:.0f}%</b>'
+            },
+            series: [{
+                name: 'Porcentaje',
+                data: [
+                    ['Parte 1', 5],
+                    ['Parte 2', 4],
+                    ['Parte 3', 3],
+                ]
+            }]
+        });
+    }
+}
