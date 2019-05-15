762945d2531c21736e893354c8c582f8aad3bef9
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-05-15 16:05:07 -0500

    Configurando database para localhost (testing)

--- app/index.js
+++ app/index.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 // Conexión a la base de datos
-let dbConn = 'mongodb://user1:123456a@ds025232.mlab.com:25232/eacidb';
-//let dbConn = "mongodb://" + config.DB_USER + ":" + config.DB_PASSWORD + "@" + config.DB_HOST;
+//let dbConn = 'mongodb://user1:123456a@ds025232.mlab.com:25232/eacidb';
+let dbConn = "mongodb://" + config.DB_USER + ":" + config.DB_PASSWORD + "@" + config.DB_HOST;
 mongoose.connect(dbConn, {useNewUrlParser: true}).then( () => {
   console.log(`Connected to ${dbConn} successfully...`);
 }).catch( err => {
