a954442263b1ec0be8871b9ed9ff046f4007b6e6
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-05-16 23:23:04 -0500

    Añadiendo MongoCloud para Sprint con Product Owner

--- app/index.js
+++ app/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const server = express();
 const routes = require('./routes/routes');
-const config = require('./config');
 const mongoose = require('mongoose');
 const path = require('path');
 
 // Conexión a la base de datos
-//let dbConn = 'mongodb://user1:123456a@ds025232.mlab.com:25232/eacidb';
-let dbConn = "mongodb://" + config.DB_USER + ":" + config.DB_PASSWORD + "@" + config.DB_HOST;
+let dbConn = 'mongodb://user1:123456a@ds025232.mlab.com:25232/eacidb';
+//let dbConn = "mongodb://" + config.DB_USER + ":" + config.DB_PASSWORD + "@" + config.DB_HOST;
 mongoose.connect(dbConn, {useNewUrlParser: true}).then( (req, res) => {
   console.log("Conectado a la base de datos exitosamente");
 }).catch( err => {
