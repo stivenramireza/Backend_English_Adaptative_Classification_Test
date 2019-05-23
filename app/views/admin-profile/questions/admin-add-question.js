acf77b26707d4cc8abfa0a2e47c6e5b7dca9b509
Juan Pablo González <jugonz31@eafit.edu.co>
2019-05-23 12:37:31 -0500

    Cambios en las posibles respuestas de los examenes, comenzando el funcionamiento de Añadir preguntas

--- app/views/admin-profile/questions/admin-add-question.js
+++ app/views/admin-profile/questions/admin-add-question.js
@@ -1,2 +1,28 @@
-var nombre_completo = localStorage.getItem('nombre_admin');
-document.getElementById("nombreAdmin").innerHTML = nombre_completo;
+let addQuestion = function () {
+    var answer1 = document.getElementById("answer1").value,  
+    answer2 = document.getElementById("answer2").value,
+    answer3 = document.getElementById("answer3").value;
+    var texto = [answer1, answer2, answer3]
+
+    var correct = document.getElementById("correctAnswer").value;
+    var difficult = document.getElementById("difficult").value;
+    
+    var http = new XMLHttpRequest();
+    http.responseType = 'json';
+    http.open("POST", "/api/register/question", true); 
+    http.setRequestHeader("Content-type", "application/json");
+    http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+            window.location.replace('/signin/candidate');
+        }
+    }
+        
+
+    http.send(JSON.stringify({
+        pregunta: document.getElementById("question").value,
+        parte: difficult,
+        dificultad: difficult,
+        texto: texto
+    }));
+
+}
