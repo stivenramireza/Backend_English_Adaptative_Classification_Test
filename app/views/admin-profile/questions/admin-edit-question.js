a6d311c3e73cc572fcc6416c269164158c4a53df
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-05-25 18:58:20 -0500

    Cambiando idioma del datatable

--- app/views/admin-profile/questions/admin-edit-question.js
+++ app/views/admin-profile/questions/admin-edit-question.js
@@ -10,6 +10,30 @@ $(document).ready(function() {
         if (http.readyState == 4 && http.status == 200) {
             var texto = http.response.preguntas;
             var table = $('#tabla_preguntas').DataTable({
+                "language": {
+                    "sProcessing":     "Procesando...",
+                    "sLengthMenu":     "Mostrar _MENU_ preguntas",
+                    "sZeroRecords":    "No se encontraron resultados",
+                    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                    "sInfo":           "Mostrando preguntas del _START_ al _END_ de un total de _TOTAL_ preguntas",
+                    "sInfoEmpty":      "Mostrando preguntas del 0 al 0 de un total de 0 preguntas",
+                    "sInfoFiltered":   "(filtrado de un total de _MAX_ preguntas)",
+                    "sInfoPostFix":    "",
+                    "sSearch":         "Buscar:",
+                    "sUrl":            "",
+                    "sInfoThousands":  ",",
+                    "sLoadingRecords": "Cargando...",
+                    "oPaginate": {
+                        "sFirst":    "Primero",
+                        "sLast":     "Último",
+                        "sNext":     "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+                    "oAria": {
+                        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                    }
+                },
                 "data": texto,
                 "columns":[
                     {"data": "n_item"},
