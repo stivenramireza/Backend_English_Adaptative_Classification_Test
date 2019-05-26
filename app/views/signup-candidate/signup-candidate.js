8e383d48c8c342f44903ffa323842fdb9f23429c
camilocruzv <camilocruzv@users.noreply.github.com>
2019-05-25 21:41:06 -0500

    Update signup-candidate.js

--- app/views/signup-candidate/signup-candidate.js
+++ app/views/signup-candidate/signup-candidate.js
@@ -40,77 +40,74 @@ let signup = function () {
         } else if (email.length < 7) {
             alertify.set('notifier', 'position', 'bottom-center');
             alertify.notify('El email debe ser mayor a 6 caracteres', 'error', 3);
-        //} else if (!j) {
-            //alertify.set('notifier', 'position', 'bottom-center');
-            //alertify.notify('El email dado no es un email válido', 'error', 3);
         } else {
-        var phone = document.getElementById("telefono").value;
-        if (typeof phone == 'number' || phone == '') {
-            console.log(typeof phone);
-            var http = new XMLHttpRequest();
-            http.responseType = 'json';
-            http.open("POST", "/api/register/candidate", true);
-            http.setRequestHeader("Content-type", "application/json");
-            http.send(JSON.stringify({
-                doctype: document.getElementById("dt").value,
-                docnumber: document.getElementById("docnumber").value,
-                firstname: document.getElementById("firstName").value,
-                lastname: document.getElementById("lastName").value,
-                genre: document.getElementById("genero").value,
-                birthdate: document.getElementById("birthday").value,
-                currentcity: document.getElementById("city").value,
-                address: document.getElementById("direccion").value,
-                phonenumber: phone,
-                mobilephonenumber: document.getElementById("celular").value,
-                email: document.getElementById("correo").value,
-                examen_activo: false
-            }));
-            console.log(http.readyState);
-            console.log(http.status);
-            console.log(http.response);
+            var phone = document.getElementById("telefono").value;
+            if (typeof phone == 'number' || phone == '') {
+                console.log(typeof phone);
+                var http = new XMLHttpRequest();
+                http.responseType = 'json';
+                http.open("POST", "/api/register/candidate", true);
+                http.setRequestHeader("Content-type", "application/json");
+                http.send(JSON.stringify({
+                    doctype: document.getElementById("dt").value,
+                    docnumber: document.getElementById("docnumber").value,
+                    firstname: document.getElementById("firstName").value,
+                    lastname: document.getElementById("lastName").value,
+                    genre: document.getElementById("genero").value,
+                    birthdate: document.getElementById("birthday").value,
+                    currentcity: document.getElementById("city").value,
+                    address: document.getElementById("direccion").value,
+                    phonenumber: phone,
+                    mobilephonenumber: document.getElementById("celular").value,
+                    email: document.getElementById("correo").value,
+                    examen_activo: false
+                }));
+                console.log(http.readyState);
+                console.log(http.status);
+                console.log(http.response);
 
-            setTimeout(function () {
-                var message = http.response.message;
-                if (http.response.status == 'failed') {
-                    if (message.includes("docnumber_1")) {
-                        alertify.set('notifier', 'position', 'bottom-center');
-                        alertify.notify('Ya existe un registro con el mismo número de documento de identidad dado', 'error', 5);
-                    } else if (message.includes("email")) {
-                        alertify.set('notifier', 'position', 'bottom-center');
-                        alertify.notify('Ya existe un registro con el mismo email dado', 'error', 5);
+                setTimeout(function () {
+                    var message = http.response.message;
+                    if (http.response.status == 'failed') {
+                        if (message.includes("docnumber_1")) {
+                            alertify.set('notifier', 'position', 'bottom-center');
+                            alertify.notify('Ya existe un registro con el mismo número de documento de identidad dado', 'error', 5);
+                        } else if (message.includes("email")) {
+                            alertify.set('notifier', 'position', 'bottom-center');
+                            alertify.notify('Ya existe un registro con el mismo email dado', 'error', 5);
+                        } else {
+                            alertify.set('notifier', 'position', 'bottom-center');
+                            alertify.notify('Ya existe un registro con el mismo número de documento de identidad o el email dado', 'error', 5);
+                        }
                     } else {
                         alertify.set('notifier', 'position', 'bottom-center');
-                        alertify.notify('Ya existe un registro con el mismo número de documento de identidad o el email dado', 'error', 5);
-                    }            
-                } else {
-                    alertify.set('notifier', 'position', 'bottom-center');
-                    alertify.notify('El registro se ha completado exitosamente', 'success', 5);
+                        alertify.notify('El registro se ha completado exitosamente', 'success', 5);
 
-                    //setTimeout(function () {
-                    //window.location.replace('/signin');
-                    //}, 2500)
+                        setTimeout(function () {
+                            window.location.replace('/signin');
+                        }, 2500)
 
-                    setTimeout(function () {
-                        document.getElementById("dt").value = "0";
-                        document.getElementById("docnumber").value = "";
-                        document.getElementById("firstName").value = "";
-                        document.getElementById("lastName").value = "";
-                        document.getElementById("birthday").value = "";
-                        document.getElementById("direccion").value = "";
-                        document.getElementById("telefono").value = "";
-                        document.getElementById("celular").value = "";
-                        document.getElementById("correo").value = "";
-                        document.getElementById("city").value = "";
-                        document.getElementById("genero").value = "0";
-                        document.getElementById("same-address").checked = null;
-                    }, 500)
-                }
-            }, 1000)
+                        setTimeout(function () {
+                            document.getElementById("dt").value = "0";
+                            document.getElementById("docnumber").value = "";
+                            document.getElementById("firstName").value = "";
+                            document.getElementById("lastName").value = "";
+                            document.getElementById("birthday").value = "";
+                            document.getElementById("direccion").value = "";
+                            document.getElementById("telefono").value = "";
+                            document.getElementById("celular").value = "";
+                            document.getElementById("correo").value = "";
+                            document.getElementById("city").value = "";
+                            document.getElementById("genero").value = "0";
+                            document.getElementById("same-address").checked = null;
+                        }, 500)
+                    }
+                }, 1000)
 
-        } else {
-            alertify.set('notifier', 'position', 'bottom-center');
-            alertify.notify('El teléfono debe ser númerico', 'error', 5);
+            } else {
+                alertify.set('notifier', 'position', 'bottom-center');
+                alertify.notify('El teléfono debe ser númerico', 'error', 5);
+            }
         }
-    }
     }
 }
