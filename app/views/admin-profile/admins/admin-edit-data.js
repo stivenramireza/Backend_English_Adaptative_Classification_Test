127bc59d71e9c54cc23cc2163db45de6fff6062f
camilocruzv <camilocruzv@users.noreply.github.com>
2019-05-26 01:50:58 -0500

    Añadiendo alertifies

--- app/views/admin-profile/admins/admin-edit-data.js
+++ app/views/admin-profile/admins/admin-edit-data.js
@@ -2,9 +2,9 @@ var nombre_completo = localStorage.getItem('nombre_admin');
 document.getElementById("nombreAdmin").innerHTML = nombre_completo;
 var http = new XMLHttpRequest();
 var docnumber = localStorage.getItem("docnumber");
-var params = 'docnumber='+docnumber;
+var params = 'docnumber=' + docnumber;
 http.responseType = 'json';
-http.open('GET', '/api/admin/edit'+'?'+ params, true);
+http.open('GET', '/api/admin/edit' + '?' + params, true);
 http.setRequestHeader("Content-type", "application/json");
 http.send(null);
 http.onreadystatechange = function () {
@@ -38,9 +38,9 @@ http.onreadystatechange = function () {
         var genero = texto.genre;
         document.getElementById("genero").value = genero;
         var estado = texto.estado;
-        if(estado){
+        if (estado) {
             document.getElementById("estado_cuenta").value = 'Activo/a';
-        }else{
+        } else {
             document.getElementById("estado_cuenta").value = 'Inactivo/a"';
         }
         var habilitar = texto.habilitar_examenes;
@@ -55,51 +55,102 @@ http.onreadystatechange = function () {
 }
 
 let update = function () {
-    var http = new XMLHttpRequest();
-    var id = localStorage.getItem('id_admin');
-    var params = 'idAdmin='+id;
-    http.responseType = 'json';
-    http.open("PUT", "/api/admin/update"+'?'+params, true); 
-    http.setRequestHeader("Content-type", "application/json");
-    http.onreadystatechange = function () {
-        if (http.readyState == 4 && http.status == 200) {
-            localStorage.setItem("mikey", http.response.token);
-            window.location.replace('/admin/profile');
+    var doctype = $("#dt").val();
+    var docnumber = $("#docnumber").val();
+    var firstname = $("#firstName").val();
+    var lastname = $("#lastName").val();
+    var username = $("#username").val();
+    var password = $("#password").val();
+    var genero = $("#genero").val();
+    var birthdate = $("#birthdate").val();
+    var city = $("#city").val();
+    var address = $("#direccion").val();
+    var phonenumber = $("#telefono").val();
+    var mobilephonenumber = $("#celular").val();
+    var email = $("#correo").val();
+
+    if (doctype == '' || docnumber == '' || firstname == '' || lastname == '' ||
+        genero == '' || birthdate == '' || city == '' || address == '' ||
+        mobilephonenumber == '' || email == '') {
+        alertify.set('notifier', 'position', 'bottom-center');
+        alertify.notify('No se han completado todos los campos obligatorios', 'error', 3);
+    } else {
+        if (docnumber.length < 5) {
+            console.log("inmelo");
+            alertify.set('notifier', 'position', 'bottom-center');
+            alertify.notify('El número de documento de identidad debe tener almenos 5 caracteres', 'error', 3);
+        } else if (firstname.length < 4) {
+            alertify.set('notifier', 'position', 'bottom-center');
+            alertify.notify('El nombre debe tener almenos 4 caracteres', 'error', 3);
+        } else if (lastname.length < 4) {
+            alertify.set('notifier', 'position', 'bottom-center');
+            alertify.notify('Los apellidos deben tener 4 caracteres', 'error', 3);
+        } else if (username.length < 4) {
+            alertify.set('notifier', 'position', 'bottom-center');
+            alertify.notify('El usuario debe tener almenos 4 caracteres', 'error', 3);
+        } else if (password.length < 8) {
+            alertify.set('notifier', 'position', 'bottom-center');
+            alertify.notify('La contraseña debe tener almenos 8 caracteres', 'error', 3);
+        } else if (address.length < 4) {
+            alertify.set('notifier', 'position', 'bottom-center');
+            alertify.notify('La dirección de residencia debe tener almenos 4 caracteres', 'error', 3);
+        } else if (mobilephonenumber.length < 12) {
+            alertify.set('notifier', 'position', 'bottom-center');
+            alertify.notify('El número de celular debe tener almenos 12 caracteres', 'error', 3);
+        } else if (email.length < 7) {
+            alertify.set('notifier', 'position', 'bottom-center');
+            alertify.notify('El email debe tener almenos 7 caracteres', 'error', 3);
+        } else {
+
+            var http = new XMLHttpRequest();
+            var id = localStorage.getItem('id_admin');
+            var params = 'idAdmin=' + id;
+            http.responseType = 'json';
+            http.open("PUT", "/api/admin/update" + '?' + params, true);
+            http.setRequestHeader("Content-type", "application/json");
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    localStorage.setItem("mikey", http.response.token);
+                    //window.location.replace('/admin/profile');
+                }
+            }
+            var habilita = document.getElementById("habilitaExamenes").checked;
+            var reactiva = document.getElementById("reactivaExamenes").checked;
+            var gestiona = document.getElementById("gestionaEstadisticas").checked;
+            var clasifica = document.getElementById("clasificaAspirantes").checked;
+            var estado_cuenta = document.getElementById("estado_cuenta").value;
+            if (estado_cuenta == 'Activo/a') {
+                estado_cuenta = true;
+            } else {
+                estado_cuenta = false;
+            }
+            if (habilita | reactiva | gestiona | clasifica) {
+                http.send(JSON.stringify({
+                    doctype: document.getElementById("dt").value,
+                    docnumber: document.getElementById("docnumber").value,
+                    estado: estado_cuenta,
+                    estado: estado_cuenta,
+                    estado: estado_cuenta,
+                    firstname: document.getElementById("firstName").value,
+                    lastname: document.getElementById("lastName").value,
+                    genre: document.getElementById("genero").value,
+                    birthdate: document.getElementById("birthdate").value,
+                    currentcity: document.getElementById("city").value,
+                    address: document.getElementById("direccion").value,
+                    phonenumber: document.getElementById("telefono").value,
+                    mobilephonenumber: document.getElementById("celular").value,
+                    email: document.getElementById("correo").value,
+                    username: document.getElementById("username").value,
+                    password: document.getElementById("password").value,
+                    habilitar_examenes: habilita,
+                    reactivar_examenes: reactiva,
+                    gestionar_estadisticas: gestiona,
+                    clasificar_aspirantes: clasifica
+                }));
+            } else {
+                alertify.set('notifier', 'position', 'bottom-center');
+                alertify.notify('No se ha asignado ningún rol', 'error', 3);
+            }
         }
-    }
-    var habilita = document.getElementById("habilitaExamenes").checked;
-    var reactiva = document.getElementById("reactivaExamenes").checked;
-    var gestiona = document.getElementById("gestionaEstadisticas").checked;
-    var clasifica = document.getElementById("clasificaAspirantes").checked;
-    var estado_cuenta = document.getElementById("estado_cuenta").value;
-    if(estado_cuenta == 'Activo/a'){
-        estado_cuenta = true;
-    }else{
-        estado_cuenta = false;
-    }
-    if(habilita | reactiva | gestiona | clasifica){
-        http.send(JSON.stringify({
-            doctype: document.getElementById("dt").value,
-            docnumber: document.getElementById("docnumber").value,
-            estado: estado_cuenta,  
-            firstname: document.getElementById("firstName").value,
-            lastname: document.getElementById("lastName").value,
-            genre: document.getElementById("genero").value,
-            birthdate: document.getElementById("birthdate").value,
-            currentcity: document.getElementById("city").value,
-            address: document.getElementById("direccion").value,
-            phonenumber: document.getElementById("telefono").value,
-            mobilephonenumber: document.getElementById("celular").value,
-            email: document.getElementById("correo").value,
-            username: document.getElementById("username").value,
-            password: document.getElementById("password").value,
-            habilitar_examenes: habilita,
-            reactivar_examenes: reactiva,
-            gestionar_estadisticas: gestiona,
-            clasificar_aspirantes: clasifica
-        }));
-    }else{
-        $('#failed').show();
-        console.log("habilita | reactiva | gestiona | clasifica is not selected");
     }
 };
