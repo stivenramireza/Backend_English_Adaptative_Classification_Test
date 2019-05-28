7b551f2da3e30cda4ffa976d166e25bb7bf4d747
angrajales <38627313+angrajales@users.noreply.github.com>
2019-05-27 23:03:27 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker kill eaciapp > /dev/null 2>&1
 docker rm eaciapp > /dev/null 2>&1
-docker run -d --name eaciapp -p 80:8000 agrajal7/eaciapp 
+docker run --env-file $PWD/env_file -d --name eaciapp -p 80:8000 agrajal7/eaciapp
