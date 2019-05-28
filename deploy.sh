90e9d7d9101f8991d38426ebfe3ed27a500a5dc1
angrajales <38627313+angrajales@users.noreply.github.com>
2019-05-27 23:27:43 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker kill eaciapp > /dev/null 2>&1
 docker rm eaciapp > /dev/null 2>&1
-env > env_file && docker run --env-file env_file -d --name eaciapp -p 80:8000 agrajal7/eaciapp
+docker run -e DB_USER=test -d --name eaciapp -p 80:8000 agrajal7/eaciapp
