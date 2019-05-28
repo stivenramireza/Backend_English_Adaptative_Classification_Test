cb3fe4ae9d27e8eb56d181056569764faffac0d9
angrajales <38627313+angrajales@users.noreply.github.com>
2019-05-27 22:54:32 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker kill eaciapp > /dev/null 2>&1
 docker rm eaciapp > /dev/null 2>&1
-docker run -d --name eaciapp -p 80:8000 agrajal7/eaciapp
+docker run -d --name eaciapp -p 80:8000 agrajal7/eaciapp -e MONGODB_URI=$MONGODB_URI -e DB_USER=$DB_USER -e DB_PASSWORD=$DB_PASSWORD
