1501bfb9ab6a23fe26996638b3033754a569c337
angrajales <38627313+angrajales@users.noreply.github.com>
2019-05-27 23:53:05 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker kill eaciapp > /dev/null 2>&1
 docker rm eaciapp > /dev/null 2>&1
-docker run -e DB_USER -e DB_PASSWORD -e "PORT=8000" -e JWT_TOKEN -e MONGODB_URI -d --name eaciapp -p 80:8000 agrajal7/eaciapp env
+export TEST1=test1
+docker run -e TEST1 -d --name eaciapp -p 80:8000 agrajal7/eaciapp env
